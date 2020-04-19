05851852cdf8041973ccee049b295af60818ec5a
ASharph <aydar3107@yandex.ru>
2020-04-19 12:44:25 +0300

    Add project files.

---
+++ SubresourceIntehrityTools/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SubresourceIntehrityTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileText = SubresourceIntegrityUtility.GetExternalScriptText("https://code.jquery.com/jquery-3.5.0.min.js", out var isCorsHeaderPresented);
+
+            if(!isCorsHeaderPresented)
+            {
+                Console.WriteLine("Access-Control-Allow-Origin does not exist in the external file. Subresource integrity cannot properly work with this script.");
+            }
+
+            Console.WriteLine(SubresourceIntegrityUtility.GenerateHashSha256(fileText));
+            Console.WriteLine(SubresourceIntegrityUtility.GenerateHashSha384(fileText));
+            Console.WriteLine(SubresourceIntegrityUtility.GenerateHashSha512(fileText));
+        }
+    }
+}
---
+++ SubresourceIntehrityTools/SubresourceIntegrityUtility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SubresourceIntehrityTools
+{
+    public static class SubresourceIntegrityUtility
+    {
+        public static string GenerateHashSha256(string text)
+        {
+            using var sha256 = SHA256.Create();
+            var byteArray = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+            return "sha256-" + Convert.ToBase64String(byteArray);
+        }
+
+        public static string GenerateHashSha384(string text)
+        {
+            using var sha384 = SHA384.Create();
+            var byteArray = sha384.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+            return "sha384-" + Convert.ToBase64String(byteArray);
+        }
+
+        public static string GenerateHashSha512(string text)
+        {
+            using var sha512 = SHA512.Create();
+            var byteArray = sha512.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+            return "sha512-" + Convert.ToBase64String(byteArray);
+        }
+
+        public static string GetExternalScriptText(string url, out bool isCorsHeaderPresented)
+        {
+            isCorsHeaderPresented = true;
+            using var response = ((HttpWebRequest)WebRequest.Create(url)).GetResponse();
+
+            if (response.Headers["Access-Control-Allow-Origin"] == null)
+            {
+                isCorsHeaderPresented = false;
+            }
+
+            using var streamReader = new StreamReader(response.GetResponseStream());
+
+            return streamReader.ReadToEnd();
+        }
+    }
+}
